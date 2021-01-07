ba60a2f125ff32f5b77710a0c20d485e946dd26e
James Dollard <j_dollard@coloradocollege.edu>
2021-01-07 14:32:22 +0000

    Merged in MMA-96-make-drag-between-memories-only-on-sides-otherwis (pull request #44)
    lower list scroll sensitivity
    
    * lower list scroll sensitivity

--- memorme_android_flutter/lib/widgets/memories_list_horizontal.dart
+++ memorme_android_flutter/lib/widgets/memories_list_horizontal.dart
@@ -1,45 +1,54 @@
-import 'package:flutter/gestures.dart';
+
 import 'package:flutter/material.dart';
 import 'package:memorme_android_flutter/data/models/memories/memory.dart';
-import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
-
 import 'memory_display.dart';
 
 /*
-* Horizontally slide between memories. Should somehow lower left/right scroll sensitivity such that a very purposeful swipe switches memory display
+* Horizontally slide between memories
 */
 
+/// modification of page scroll physics that lowers scroll sensitivity
+/// by increasing drag start distance
+class LowSensitivityScrollPhysics extends PageScrollPhysics {
+
+  const LowSensitivityScrollPhysics({ScrollPhysics parent}) 
+      : super(parent: parent);
+
+  @override
+  double get dragStartDistanceMotionThreshold => 40;
+
+  @override
+  LowSensitivityScrollPhysics applyTo(ScrollPhysics ancestor) {
+    return LowSensitivityScrollPhysics(parent: buildParent(ancestor));
+  }
+}
+
+
+/// displays a list of [memories] that jumps to [focusedIndex]
+/// with sensitivity determined by [physics]
 class MemoriesList extends StatelessWidget {
   final List<Memory> memories;
   final int focusedIndex;
-  final PageController pageController = PageController();
+  final PageController pageController;
+  final ScrollPhysics physics = LowSensitivityScrollPhysics();
 
   MemoriesList(
       {Key key,
       List<Memory> memories,
       this.focusedIndex})
       : memories = memories ?? const [],
+        this.pageController = PageController(initialPage: focusedIndex),
         super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    //not sure if this is the best place for this function call
-    //but basically allows scrolling to widget
-    WidgetsBinding.instance
-        .addPostFrameCallback((_) => _executeAfterWholeBuildProcess(context));
 
     return PageView(
+        physics: physics,
         controller: pageController,
         children: [
           for (Memory m in memories) ListView(children: [MemoryDisplay(m)],)
         ],
     );
-  }
-
-  //to be called internally; scrolls to element if needed
-  _executeAfterWholeBuildProcess(BuildContext context) {
-    if (this.memories.length > 0 && focusedIndex != null) {
-      pageController.jumpToPage(focusedIndex);
-    }
   }
 }
